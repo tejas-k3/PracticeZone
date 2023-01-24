11dc8f866c39c39d74bbd183a069f35612a2b9f1
Tejas <tejaskothari03@gmail.com>
2023-01-24 08:14:29 +0530

    Valid Sudoku sol

---
+++ ValidSudoku.py
@@ -0,0 +1,37 @@
+""" https://leetcode.com/problems/valid-sudoku/
+Approach :- Assign hash value to each cell as per
+its box, row number, col number and validate it's
+unique and if not, return False.
+"""
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9
+
+        # Use hash set to record the status
+        rows = [set() for _ in range(N)]
+        cols = [set() for _ in range(N)]
+        boxes = [set() for _ in range(N)]
+
+        for r in range(N):
+            for c in range(N):
+                val = board[r][c]
+                if val == ".":
+                    continue
+
+                # Check the row
+                if val in rows[r]:
+                    return False
+                rows[r].add(val)
+
+                # Check the column
+                if val in cols[c]:
+                    return False
+                cols[c].add(val)
+
+                # Check the box
+                idx = (r // 3) * 3 + c // 3
+                if val in boxes[idx]:
+                    return False
+                boxes[idx].add(val)
+
+        return True
