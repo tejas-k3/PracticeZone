eb98fe252bbe68c814ff88929261854e2b6356cd
Tejas <tejaskothari03@gmail.com>
2023-04-29 00:35:42 +0530

    [Java] Group Anagrams sol

---
+++ GroupAnagrams.java
@@ -0,0 +1,27 @@
+/*
+ * 
+ * 
+ */
+import java.util.*;
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs)
+    {
+        Map<String, List<String>> groups = new HashMap<>();
+        for (String s:strs) {
+            int[] count = new int[26];
+            for (char c:s.toCharArray()) {
+                count[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i<26;i++)
+            {
+                sb.append(count[i]);
+                sb.append('#');
+            }
+            String key = sb.toString();
+            groups.putIfAbsent(key, new ArrayList<>());
+            groups.get(key).add(s);
+        }
+        return new ArrayList<>(groups.values());
+    }
+}
