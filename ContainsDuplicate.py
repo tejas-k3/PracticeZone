b7ee649c462f1cf6f16f1cf48c28a3fb64130e2e
Tejas <tejaskothari03@gmail.com>
2023-04-28 23:15:29 +0530

    [Py] Contains duplicate sol

---
+++ ContainsDuplicate.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
