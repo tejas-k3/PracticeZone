3ed140f24f3c60e2977478b3e0203d67a7d93981
Tejas <tejaskothari03@gmail.com>
2023-04-30 23:33:43 +0530

    [Java] Tow Sum II sol

---
+++ TwoSumII.java
@@ -0,0 +1,12 @@
+public class TwoSumII {
+    public int[] twoSum(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        
+        while (nums[l] + nums[r] != target) {
+            if (nums[l] + nums[r] < target) l++;
+            else r--;
+        }
+    
+        return new int[] {l+1, r+1};
+    }
+}
