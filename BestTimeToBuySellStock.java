96c11687acda36e05d2554553afc96945f2e6900
Tejas <tejaskothari03@gmail.com>
2023-05-01 15:02:12 +0530

    [Java] Best Time to Buy Sell Stock sol

---
+++ BestTimeToBuySellStock.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+public class BestTimeToBuySellStock {
+        public int maxProfit(int prices[]) {
+            int minprice = Integer.MAX_VALUE;
+            int maxprofit = 0;
+            for (int i = 0; i < prices.length; i++) {
+                if (prices[i] < minprice)
+                    minprice = prices[i];
+                else if (prices[i] - minprice > maxprofit)
+                    maxprofit = prices[i] - minprice;
+            }
+            return maxprofit;
+        }
+    }
