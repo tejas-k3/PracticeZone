45060adb4a7781f2fa77da4c627586c9195043e7
Tejas <tejaskothari03@gmail.com>
2023-05-04 08:22:48 +0530

    [Py] Daily Temperatures sol

---
+++ DailyTemperatures.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/daily-temperatures/
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        hottest = 0
+        answer = [0] * n
+        
+        for curr_day in range(n - 1, -1, -1):
+            current_temp = temperatures[curr_day]
+            if current_temp >= hottest:
+                hottest = current_temp
+                continue
+            
+            days = 1
+            while temperatures[curr_day + days] <= current_temp:
+                # Use information from answer to search for the next warmer day
+                days += answer[curr_day + days]
+            answer[curr_day] = days
+
+        return answer
