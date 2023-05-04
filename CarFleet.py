72a37314cf244871744c5b12c8b0a0464584dac4
Tejas <tejaskothari03@gmail.com>
2023-05-05 02:40:33 +0530

    [Py] Car Fleet sol

---
+++ CarFleet.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/car-fleet/
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        time = [float(target - p) / s for p, s in sorted(zip(position, speed))]
+        res = cur = 0
+        for t in time[::-1]:
+            if t > cur:
+                res += 1
+                cur = t
