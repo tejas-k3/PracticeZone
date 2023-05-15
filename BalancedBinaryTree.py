4760cb034e44b7d44a721c76376501ec46e5c7ad
Tejas <tejaskothari03@gmail.com>
2023-05-15 13:52:18 +0530

    [Py] Balanced Binary Tree sol

---
+++ BalancedBinaryTree.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Return whether or not the tree at root is balanced while also returning
+    # the tree's height
+    def isBalancedHelper(self, root: TreeNode) -> (bool, int):
+        # An empty tree is balanced and has height -1
+        if not root:
+            return True, -1
+        # Check subtrees to see if they are balanced. 
+        leftIsBalanced, leftHeight = self.isBalancedHelper(root.left)
+        if not leftIsBalanced:
+            return False, 0
+        rightIsBalanced, rightHeight = self.isBalancedHelper(root.right)
+        if not rightIsBalanced:
+            return False, 0
+        # If the subtrees are balanced, check if the current tree is balanced
+        # using their height
+        return (abs(leftHeight - rightHeight) < 2), 1 + max(leftHeight, rightHeight)
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.isBalancedHelper(root)[0]
