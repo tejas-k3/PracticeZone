9fec6d1b63a295bab4e9665f2d7220452d3c0d20
Tejas <tejaskothari03@gmail.com>
2023-05-19 00:14:56 +0530

    [Py] Lowest common ancestor BST

---
+++ LowestCommonAncestorOfBST.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# 
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        p_val = p.val
+        q_val = q.val
+        node = root
+        while node:
+            parent_val = node.val
+            if p_val > parent_val and q_val > parent_val:    
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
+            else:
+                # We have found the split point, i.e. the LCA node.
+                return node
