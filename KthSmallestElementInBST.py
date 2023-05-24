08ebcdc342cedd6a78929ef8b29cc7a4e4dcc551
Tejas <tejaskothari03@gmail.com>
2023-05-25 00:27:39 +0530

    Kth Smallest Element in BST sol

---
+++ KthSmallestElementInBST.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
