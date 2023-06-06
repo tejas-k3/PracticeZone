96c703f44e146854d1aeceb368ce27e78ea9776d
Tejas <tejaskothari03@gmail.com>
2023-06-07 00:29:13 +0530

    [Java] Letter Combination Of Phone Numbers

---
+++ LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// Backtracking
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+class Solution {
+    private List<String> combinations = new ArrayList<>();
+    private Map<Character, String> letters = Map.of(
+        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
+        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+    private String phoneDigits;
+    
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        phoneDigits = digits;
+        backtrack(0, new StringBuilder());
+        return combinations;
+    }
+    
+    private void backtrack(int index, StringBuilder path) {
+        // If the path is the same length as digits, we have a complete combination
+        if (path.length() == phoneDigits.length()) {
+            combinations.add(path.toString());
+            return; // Backtrack
+        }
+        // Get the letters that the current digit maps to, and loop through them
+        String possibleLetters = letters.get(phoneDigits.charAt(index));
+        for (char letter: possibleLetters.toCharArray()) {
+            path.append(letter);
+            // Move on to the next digit
+            backtrack(index + 1, path);
+            // Backtrack by removing the letter before moving onto the next
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+}
