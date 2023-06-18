91e0625ce93c6a550638d853d787961231d8df10
Tejas <tejaskothari03@gmail.com>
2023-06-19 01:16:37 +0530

    [Py] Coin Change sol

---
+++ CoinChange.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/coin-change/
+# 1D DP, I hate this problem specifically
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1 
