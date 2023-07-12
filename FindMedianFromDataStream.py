36f1c23d0663effba856b90af8a148f8029f3867
Tejas <tejaskothari03@gmail.com>
2023-07-13 00:01:28 +0530

    [Py] Find median form data stream sol

---
+++ FindMedianFromDataStream.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/find-median-from-data-stream/
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.low=[]
+        self.high=[]
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.low, -num)
+        heapq.heappush(self.high, -heapq.heappop(self.low))
+        if len(self.low) < len(self.high):
+            heapq.heappush(self.low, -heapq.heappop(self.high))
+
+    def findMedian(self) -> float:
+        if len(self.low) > len(self.high):
+            return -self.low[0]
+        else:
+            return float(-self.low[0] + self.high[0]) / 2
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
