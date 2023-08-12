f8f8b789432e7ae16568cb12fc3a7cf399bb76a1
Tejas <tejaskothari03@gmail.com>
2023-08-12 13:24:04 +0530

    [Py] Added 2 pointers Approach to 3Sum

--- 3Sum.py
+++ 3Sum.py
@@ -12,3 +12,28 @@ class Solution:
                         res.add(tuple(sorted((val1, val2, complement))))
                     seen[val2] = i
         return res
+
+"""
+Another sol of this as done in 2Sum Sorted!
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        seen = {}
+        nums.sort()
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+            l, r = i+1, len(nums)-1
+            while l < r:
+                threeSum = val1 + nums[l] + nums[r]
+                if threeSum == 0:
+                    res.append([a, nums[l], nums[r]])
+                    l+=1
+                    while l < r and nums[l] == nums[l+1]:
+                        l+=1
+                elif threeSum > 0:
+                    r-=1
+                else:
+                    l+=1
+        return res
+
+"""
