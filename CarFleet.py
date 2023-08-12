69dbb473f595ba4aaebd517acccdfb3d9b86703e
Tejas <tejaskothari03@gmail.com>
2023-08-12 21:37:07 +0530

    [Py] Car Fleet using stack with same maths formula

--- CarFleet.py
+++ CarFleet.py
@@ -13,3 +13,16 @@ class Solution(object):
             if t > cur:
                 res += 1
                 cur = t
+""" Same approach using stack
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+"""
