bb67ed64d4a46047fbc7d2c24194358787d3700b
Tejas <tejaskothari03@gmail.com>
2023-08-18 18:34:17 +0530

    [Py] Target Sum sol

---
+++ TargetSum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/target-sum
+class Solution:
+    def findTargetSumWays(self, nums, target):
+        dp = {}
+        def backtrack(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            if (i, total) in dp:
+                return dp[(i, total)]
+            dp[(i, total)] = backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i])
+            return dp[(i, total)]
+        return backtrack(0, 0)
