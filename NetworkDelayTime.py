831f2d22ca61a672e29d78bc4b57f5995d6b2dc2
Tejas <tejaskothari03@gmail.com>
2023-08-21 18:08:47 +0530

    [Py] Network Delay Time sol

---
+++ NetworkDelayTime.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/network-delay-time/
+# O(E * logV)
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        edges = collections.defaultdict(list)
+        for u, v, w in times:
+            edges[u].append((v, w))
+        minHeap = [(0, k)]
+        visit = set()
+        t = 0
+        while minHeap:
+            w1, n1 = heapq.heappop(minHeap)
+            if n1 in visit:
+                continue
+            visit.add(n1)
+            t = w1
+            for n2, w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minHeap, (w1 + w2, n2))
+        return t if len(visit) == n else -1
