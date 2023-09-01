52c162bdea13d26e643a8ef907dac44cc4884626
Tejas <tejaskothari03@gmail.com>
2023-09-01 14:29:35 +0530

    [Py] Counting Bits proper better approach

--- CountingBit.py
+++ CountingBit.py
@@ -4,6 +4,28 @@ Approach - CHEATING, just convert using internal function
 Can do //2 tho!
 """
 
+"""
+Better approach with O(N)
+    def countBits(self, n):
+        res = [0] * (n + 1)
+        for i in range(n + 1):
+            res[i] = self.solve(i, res)
+        return res
+
+    def solve(self, n, memo):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if memo[n] != 0:
+            return memo[n]
+        if n % 2 == 0:
+            memo[n] = self.solve(n // 2, memo)
+            return memo[n]
+        else:
+            memo[n] = 1 + self.solve(n // 2, memo)
+            return memo[n]
+"""
 class Solution:
     def countBits(self, n: int) -> List[int]:
         nums=[]
