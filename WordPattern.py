33288a29560f1b97719bfaed23cce3391f6894b8
Tejas <tejaskothari03@gmail.com>
2023-09-03 23:58:57 +0530

    [Py] Word Pattern sol

---
+++ WordPattern.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/word-pattern
+class Solution:
+    def wordPattern(self, pattern, str):
+        s = pattern
+        t = str.split()
+        return len(set(zip(s, t))) == len(set(s)) == len(set(t)) and len(s) == len(t)
