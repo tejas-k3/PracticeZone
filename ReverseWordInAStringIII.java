1524567dd97a1eb6b5fae2ed6096a038ba6011e9
Tejas <tejaskothari03@gmail.com>
2023-10-01 09:53:19 +0530

    [Java] Reverse Word in string III

---
+++ ReverseWordInAStringIII.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii
+class Solution {
+    public String reverseWords(String s) {
+        int lastSpaceIndex = -1;
+        char[] chArray = s.toCharArray();
+        int len = s.length();
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || chArray[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+                while (startIndex < endIndex) {
+                    char temp = chArray[startIndex];
+                    chArray[startIndex] = chArray[endIndex];
+                    chArray[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = strIndex;
+            }
+        }
+        return new String(chArray);
+    }
+
+}
