243be7998bbab047b82b937224864b6178f08ec8
Tejas <tejaskothari03@gmail.com>
2023-10-09 08:59:06 +0530

    [Py] find-first-and-last-position-of-element

---
+++ FindFirstAndLastPositionOfElementInSortedArray.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return [-1, -1]
+        def bisect_left(nums, target):
+            l, r = 0, len(nums) - 1
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m
+            return l if nums[l] == target else -1
+        def bisect_right(nums, target):
+            l, r = 0, len(nums) - 1
+            while l < r:
+                m = (l + r) // 2 + 1
+                if nums[m] > target:
+                    r = m - 1
+                else:
+                    l = m
+            return l if nums[l] == target else -1
+        return [bisect_left(nums, target), bisect_right(nums, target)]
