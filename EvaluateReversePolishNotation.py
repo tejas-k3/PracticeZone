03e63d426bc56cd4ae9eec3b561ea651f11a4f5e
Tejas <tejaskothari03@gmail.com>
2023-10-12 22:21:58 +0530

    [Py] Eval RPN better code sol

--- EvaluateReversePolishNotation.py
+++ EvaluateReversePolishNotation.py
@@ -6,27 +6,43 @@ pop last 2 values from stack and push the resulted value.
 """
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-        val = 0
-        
-        for i in tokens:
-            if i == '+':
-                val =(int(stack.pop()) + int(stack.pop()))
-                stack.append(val)
-            elif i == '-':
-                t = int(stack.pop())
-                v = int(stack.pop())
-                val =(v - t)
-                stack.append(val)
-            elif i == '/':
-                t = int(stack.pop())
-                v = int(stack.pop())
-                val =int(v/t)
-                stack.append(val)
-            elif i == '*':
-                val =(int(stack.pop()) * int(stack.pop()))
-                stack.append(val)
-            else :
-                stack.append(i)
-        
-        return int(stack.pop())
+        signs = ('+', '-', '*', '/')
+        stack = list()
+        for token in tokens:
+            if token in signs:
+                y = stack.pop()
+                x = stack.pop()
+                if token == '+':
+                    token = x + y
+                elif token == '-':
+                    token = x - y
+                elif token == '/':
+                    token = x/y
+                else:
+                    token = x*y
+            stack.append(int(token))
+        return stack.pop()
+
+    # def evalRPN(self, tokens: List[str]) -> int:
+    #     stack = []
+    #     val = 0
+    #     for i in tokens:
+    #         if i == '+':
+    #             val =(int(stack.pop()) + int(stack.pop()))
+    #             stack.append(val)
+    #         elif i == '-':
+    #             t = int(stack.pop())
+    #             v = int(stack.pop())
+    #             val =(v - t)
+    #             stack.append(val)
+    #         elif i == '/':
+    #             t = int(stack.pop())
+    #             v = int(stack.pop())
+    #             val =int(v/t)
+    #             stack.append(val)
+    #         elif i == '*':
+    #             val =(int(stack.pop()) * int(stack.pop()))
+    #             stack.append(val)
+    #         else :
+    #             stack.append(i)
+    #     return int(stack.pop())
