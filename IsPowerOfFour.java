dc894a66df9a02223007f36f2a21af643b849ce5
Tejas <tejaskothari03@gmail.com>
2023-10-23 20:53:43 +0530

    [Java] Is power of 4 sol

---
+++ IsPowerOfFour.java
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/power-of-four
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
+    }
+}
